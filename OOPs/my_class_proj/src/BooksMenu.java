27bfb1261be8c2f44a06c141c6eb60f626270596
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-04-11 01:09:19 +0530

    compare price

--- OOPs/my_class_proj/src/BooksMenu.java
+++ OOPs/my_class_proj/src/BooksMenu.java
@@ -6,8 +6,9 @@ public class BooksMenu {
         Book[] books = new Book[10];
         int bkIdx = 0;
         while (true) {
-            System.out.println("Press 1 to view books, 2 to add books, any other key to exit");
+            System.out.println("Press 1 to view books, 2 to add books, 3 to compare prices, any other key to exit");
             String userAction = scanner.nextLine();
+            System.out.println("User Action is" + userAction);
             if (userAction.equals("1")) {
                 for (int i = 0; i < books.length; i++) {
                     if (books[i] != null) {
@@ -32,16 +33,41 @@ public class BooksMenu {
                 books[bkIdx++] = bkTmp;
 
             } else if (userAction.equals("3")) {
-                for (int i = 0; i < books.length; i++) {
+                int i = 0;
+                int flag = (books[0] != null) ? 1 : 0;
+                if (flag == 0) {
+                    System.out.println("No books buddy");
+                    continue;
+                }
+                while (i < books.length) {
                     if (books[i] != null) {
                         System.out.println(i + "->" + books[i].getTitle());
                     }
-                    System.out.println("Enter 2 book indexes to Compare");
-                    System.out.println("Enter first index: ");
-
-                    System.out.println("Enter second index: ");
+                    i++;
                 }
+                System.out.println("Enter first index: ");
+                int a = scanner.nextInt();
+                System.out.println("Enter second index: ");
+                int b = scanner.nextInt();
+                BookAccess.comparePrices(books[a], books[b]);
             }
+            // Alternate way // Option 3: Compare prices of two books
+            // else if (userAction.equals("3")) {
+            // // Prompt the user for the indices of the books to compare
+            // System.out.println("Enter index of first book to compare");
+            // int book1Idx = Integer.parseInt(scanner.nextLine());
+            // System.out.println("Enter index of second book to compare");
+            // int book2Idx = Integer.parseInt(scanner.nextLine());
+            // // Check if the selected indices contain valid Book objects
+            // if (books[book1Idx] != null && books[book2Idx] != null) {
+            // // Compare the books and print the details of the more expensive book
+            // System.out.println("The details of expensive book is \n" +
+            // getExpensiveBook(books[book1Idx], books[book2Idx]));
+            // } else {
+            // System.out.println("One of the books is null"); // Error message if an index
+            // is empty
+            // }
+            // }
 
             else {
                 break;
