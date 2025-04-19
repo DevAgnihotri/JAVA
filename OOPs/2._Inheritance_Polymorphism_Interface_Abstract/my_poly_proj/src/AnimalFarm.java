e63e23c8e457fc01a9c34e9170aefa0201993752
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-04-19 20:02:07 +0530

    Updated

--- OOPs/2._Inheritance_Polymorphism_Interface_Abstract/my_poly_proj/src/AnimalFarm.java
+++ OOPs/2._Inheritance_Polymorphism_Interface_Abstract/my_poly_proj/src/AnimalFarm.java
@@ -1,13 +1,51 @@
+import java.util.Scanner;
+
 public class AnimalFarm {
     public static void main(String args[]) {
-        // Creating objects
-        Animal animal1 = new Dog("Sami");
-        Animal animal2 = new Cat("Hershey");
-        Animal animal3 = new Cow("Molly");
+        Scanner sc = new Scanner(System.in);
+        Animal[] animals = new Animal[10];
+        int anmlIdx = 0;
 
-        // Printing
-        System.out.println("animal1 sound" + animal1.sound());
-        System.out.println("animal2 sound" + animal2.sound());
-        System.out.println("animal3 sound" + animal3.sound());
+        while (true) {
+            System.out.println("Press 1 to view the animals, " +
+                    "\n2 to add animals, " +
+                    "\nany other key to exit");
+            String userAction = sc.nextLine();
+            if (userAction.equals("1")) {
+                for (int i = 0; i < animals.length; i++) {
+                    if (animals[i] != null)
+                        System.out.println(animals[i]);
+                }
+            } else if (userAction.equals("2")) {
+                if (anmlIdx == 10) {
+                    System.out.println("10 Animals already added can't add more");
+                    continue;
+                }
+                System.out.println("Which animal do you want to create? \nPress 1 for dog," +
+                        "\n2 for cat " +
+                        "\n3 for cow");
+                String animalChoice = sc.nextLine();
+                if (animalChoice.equals("1")) {
+                    System.out.println("Enter the name of the dog");
+                    String dogName = sc.nextLine();
+                    Animal anmlTmp = new Dog(dogName);
+                    animals[anmlIdx++] = anmlTmp;
+                } else if (animalChoice.equals("2")) {
+                    System.out.println("Enter name of the cat");
+                    String catName = sc.nextLine();
+                    Animal anmlTmp = new Cat(catName);
+                    animals[anmlIdx++] = anmlTmp;
+                } else if (animalChoice.equals("3")) {
+                    System.out.println("Enter name of the cow");
+                    String cowName = sc.nextLine();
+                    Animal anmlTmp = new Cat(cowName);
+                    animals[anmlIdx++] = anmlTmp;
+                } else {
+                    System.out.println("Wrong Choice Senor");
+                    break;
+                }
+            }
+        }
+
     }
 }
